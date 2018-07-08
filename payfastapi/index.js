87ca99aa67d9dad87371f3aa621189c9de954949
Gil Costa <gil-real@hotmail.com>
2018-07-08 00:43:20 -0300

    criado a primeira rota (/pagamentos)

--- payfastapi/index.js
+++ payfastapi/index.js
@@ -4,5 +4,10 @@ var express = require("express");
 var app = express();
 
 app.listen(3000, function(){
-    console.log("Servidor rodando");
+    console.log("Servidor rodando na porta 3000");
+});
+
+app.get("/pagamentos", function(req, res){
+    console.log("Requisição recebida com sucesso.");
+    res.send("OK");
 });
